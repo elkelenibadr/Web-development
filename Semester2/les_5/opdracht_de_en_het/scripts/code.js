4e7c03d65ab1546dc5fb376f8aa1763062fcf06b
elkelenibadr <badr.elkeleni@student.vives.be>
2024-03-13 15:50:01 +0100

    Klaar met opdracht de en het labo 5

--- Semester2/les_5/opdracht_de_en_het/scripts/code.js
+++ Semester2/les_5/opdracht_de_en_het/scripts/code.js
@@ -1,3 +1,22 @@
 const setup = () => {
+    let zin = "Gisteren zat de jongen op de stoep en at de helft van de appel";
+    let uitvoer = vervangDeDoorHet(zin);
+    console.log(uitvoer);
 }
+
+const vervangDeDoorHet = (tekst) => {
+    let woorden = tekst.split(' ');
+
+    for (let i = 0; i < woorden.length; i++) {
+        if (woorden[i].toLowerCase() === 'de') {
+            if (woorden[i - 1]) {
+                woorden[i] = 'het' + woorden[i].slice(2);
+            } else {
+                woorden[i] = 'het';
+            }
+        }
+    }
+    return woorden.join(' ');
+}
+
 window.addEventListener("load", setup);
