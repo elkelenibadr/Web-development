dc8da9fda63240638a48070363d84f4a22be2861
elkelenibadr <badr.elkeleni@student.vives.be>
2024-04-01 15:58:38 +0200

    Klaar met labo 7
    Labo 7

---
+++ Semester2/les_7/opdracht_matching_game/scripts/code.js
@@ -0,0 +1,102 @@
+let global = {
+    AANTAL_HORIZONTAAL: 4,
+    AANTAL_VERTICAAL: 3,
+    AANTAL_KAARTEN: 6,
+    lijstImages: ["img/kaart1.png","img/kaart2.png","img/kaart3.png","img/kaart4.png","img/kaart5.png","img/kaart6.png"],
+    lijst: [],
+    lijstVoorkanten: []
+};
+
+const setup = () => {
+    let alleKaarten = document.getElementById("alleKaarten");
+
+    alleKaarten.style.display = "grid";
+    alleKaarten.style.gridTemplateColumns="repeat(" + global.AANTAL_HORIZONTAAL + ",165px)";
+    alleKaarten.style.gridTemplateRows="repeat(" + global.AANTAL_VERTICAAL + ",220px)";
+    alleKaarten.style.padding = "10px";
+    alleKaarten.style.width = "640px";
+    alleKaarten.style.height = "620px";
+    alleKaarten.style.border = "5px dashed black";
+
+    global.lijst = alleKaarten.querySelectorAll("img")
+    for (let i = 0; i < global.lijst.length; i++) {
+        global.lijst[i].setAttribute("src", "img/achterkant.png");
+    }
+
+    let teller = 0
+
+    while(teller < 12)
+    {
+        let getal = Math.round(Math.random()*5);
+        let counter = 0;
+
+        for (let i = 0; i < global.lijstVoorkanten.length; i++) {
+            if (global.lijstVoorkanten[i] === global.lijstImages[getal]) {
+                counter++;
+            }
+        }
+
+        if (counter < 2) {
+            global.lijstVoorkanten[teller] = global.lijstImages[getal];
+            teller++;
+        }
+    }
+
+    for (let i = 0; i < global.lijst.length; i++) {
+        global.lijst[i].addEventListener("click", () => {
+            handleClick(i);
+        });
+    }
+}
+
+const handleClick = (getal) =>
+{
+    global.lijst[getal].setAttribute("src", global.lijstVoorkanten[getal]);
+
+    let kaarten = [];
+
+    for (let i = 0; i < global.lijstVoorkanten.length; i++) {
+        if(global.lijst[i].getAttribute("src") !== "img/achterkant.png") {
+            kaarten.push(i);
+        }
+    }
+
+    if(kaarten.length > 1) {
+        let alleKaarten = document.getElementById("alleKaarten");
+
+        if (global.lijstVoorkanten[kaarten[0]] === global.lijstVoorkanten[kaarten[1]]) {
+            alleKaarten.style.border = "5px dashed green";
+
+            setTimeout(() => {
+                for (let i = 0; i < 2; i++) {
+                    global.lijst[kaarten[i]].setAttribute("src", "img/achterkant.png");
+                    global.lijst[kaarten[i]].style.visibility = 'hidden';
+                }
+                alleKaarten.style.border = "5px dashed black";
+            }, 1000);
+        } else {
+            alleKaarten.style.border= "5px dashed red";
+            setTimeout(() => {
+                global.lijst[kaarten[0]].setAttribute("src", "img/achterkant.png")
+                global.lijst[kaarten[1]].setAttribute("src", "img/achterkant.png")
+                alleKaarten.style.border= "5px dashed black";
+            },1000);
+        }
+    }
+
+    setTimeout(() => {
+        let hiddenCardsCount = 0;
+
+        for (let i = 0; i < global.lijst.length; i++) {
+            let style = window.getComputedStyle(global.lijst[i]);
+            if (style.getPropertyValue("visibility") === "hidden") {
+                hiddenCardsCount++;
+            }
+        }
+
+        if (hiddenCardsCount === global.lijst.length) {
+            window.alert("Je hebt gewonnen!");
+        }
+    }, 1000);
+}
+window.addEventListener("load", setup);
