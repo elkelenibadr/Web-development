e578a7b4372e0dffc1a3944e6f868eb965e65128
elkelenibadr <badr.elkeleni@student.vives.be>
2024-04-17 15:17:55 +0200

    Klaar met opdracht verjaardag
    Labo 8

--- Semester2/les_8/opdracht_verjaardag/scripts/code.js
+++ Semester2/les_8/opdracht_verjaardag/scripts/code.js
@@ -2,7 +2,7 @@ const setup = () => {
     let verjaardag = new Date('Augustus 16, 2003');
     let d = new Date();
     let verschilTijd = d.getTime() - verjaardag.getTime();
-    let verschilDagen = Math.round(verschilTijd / (1000 * 60 * 60 * 24));
+    let verschilDagen = Math.floor(verschilTijd / (1000 * 60 * 60 * 24));
     console.log(verschilDagen);
 }
 
