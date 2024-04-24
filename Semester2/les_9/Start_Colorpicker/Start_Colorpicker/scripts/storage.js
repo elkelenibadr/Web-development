546164cb9281941097ef844bc7192f7317566c0e
elkelenibadr <badr.elkeleni@student.vives.be>
2024-04-24 17:07:58 +0200

    Bezig met uitbreiding colorpicker
    Labo 9

--- Semester2/les_9/Start_Colorpicker/Start_Colorpicker/scripts/storage.js
+++ Semester2/les_9/Start_Colorpicker/Start_Colorpicker/scripts/storage.js
@@ -1,18 +1,28 @@
 
 
-const storeSliderValues = () => {
-
+const storeSliderValues = (red, green, blue) => {
+    localStorage.setItem("SliderValues", JSON.stringify(red, green, blue));
 };
 
-const restoreSliderValues = () => {
-
+const restoreSliderValues = (red, green, blue) => {
+    let slidervalues = JSON.parse(localStorage.getItem("SliderValues"));
+    if(slidervalues){
+        document.getElementById("sldRed").value = slidervalues.red;
+        document.getElementById("sldGreen").value = slidervalues.green;
+        document.getElementById("sldBlue").value = slidervalues.blue;
+    }
 };
 
 const storeSwatches = () => {
-    // bouw een array met kleurinfo objecten
+// bouw een array met kleurinfo objecten
+    let swatches = document.querySelectorAll(".swatch");
+    let swatchData = [];
+    for(let i = 0; i < swatches.length; i++){
 
+    }
 };
 
 const restoreSwatches = () => {
 
 };
+
