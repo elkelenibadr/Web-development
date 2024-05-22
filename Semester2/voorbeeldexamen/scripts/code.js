bfe5131bb5843318b44827f3d7b44f2ef03d761c
elkelenibadr <badr.elkeleni@student.vives.be>
2024-05-22 15:09:16 +0200

    Leren voor het examen
    webdev

--- Semester2/voorbeeldexamen/scripts/code.js
+++ Semester2/voorbeeldexamen/scripts/code.js
@@ -1,54 +1,77 @@
 let global = {
     woorden: ["vives", "hotel", "stoel", "rommel", "vogel"],
-    teRadenWoord: [],
+    teRadenWoord: "",
     highscores: [],
+    aantalPogingen: 0,
+    counter: 0
 };
 
 const setup = () => {
     let gok = document.getElementById("gok");
     let goButton = document.getElementById("go");
+    let nieuwspel = document.getElementById("nieuw");
 
     gok.disabled = true;
     goButton.disabled = true;
 
-    getElements().nieuwspel.addEventListener("click", () => {
-        naamIngeven();
-    });
+    nieuwspel.addEventListener("click", naamIngeven);
 
     highscoresWeergeven();
 };
 
 const getElements = () => ({
     button: document.getElementById("clear"),
-    nieuwspel: document.getElementById("nieuw"),
     helpHidden: document.getElementsByClassName("help hidden"),
-    gokken: document.getElementById("gokken"),
     game: document.getElementById("game")
 });
 
 const naamIngeven = () => {
     const input = prompt("Naam speler:");
     naamToevoegen(input);
+
+    let gok = document.getElementById("gok");
+    let goButton = document.getElementById("go");
+
+    gok.disabled = false;
+    goButton.disabled = false;
+
     if(input === "") {
         window.stop();
     }
 };
 
 const woordIngeven = () => {
-    let goButton = document.getElementById("go");
     let gok = document.getElementById("gok");
 
-    goButton.addEventListener("click", () => {
-          if(gok.index === 4) {
-              let groteDiv = document.createElement("div");
+    if(gok.value.length !==5) {
+        return;
+    }
 
-              let div1 = document.createElement("div");
-              let div2 = document.createElement("div");
-              let div3 = document.createElement("div");
-              let div4 = document.createElement("div");
-              let div5 = document.createElement("div");
-          }
-      });
+    let outer = document.createElement('div');
+
+    for(let i = 0; i < 5; i++) {
+        if(global.teRadenWoord.charAt(i) === gok.value.charAt(i)) {
+            let inner = document.createElement('div');
+            inner.classList.add('juist');
+            inner.innerText = gok.value.charAt(i);
+            outer.appendChild(inner);
+            global.counter++;
+        }
+        else if(global.teRadenWoord.indexOf(gok.value.charAt(i))) {
+            let inner = document.createElement('div');
+            inner.classList.add('bevat');
+            inner.innerText = gok.value.charAt(i);
+            outer.appendChild(inner);
+            global.counter++;
+        }
+        else {
+            let inner = document.createElement('div');
+            inner.classList.add('fout');
+            inner.innerText = gok.value.charAt(i);
+            outer.appendChild(inner);
+            global.counter++;
+        }
+    }
 };
 
 const DatumBerekenen = () => {
